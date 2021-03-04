9a105ed98e90fcd6f6878e2ad4b49153425a1c44
Yakov Volodin <ysvolodin@gmail.com>
2021-03-04 11:43:53 +0300

    ФТ проверяет возможность ввести элемент to-do-list

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,8 @@
-from selenium import webdriver
+import time
 import unittest
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 class NewVisitorTest(unittest.TestCase):
     '''Тест нового пользователя'''
@@ -20,22 +23,37 @@ class NewVisitorTest(unittest.TestCase):
 
         # Видим, что заголовок и шапка страницы говорят о to-do-list
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Закончить тест')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # Предложение ввести элемент списка
-
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Введите задание в to-do лист'
+        )
         # Ввод в текстовом поле "simple test request"
+        inputbox.send_keys('simple test request')
 
-        # При подтверждении страница обновляется и теперь она содержит "simple test request" в качестве элемента списка
+        # При нажатии enter страница обновляется и теперь она содержит "simple test request" в качестве элемента списка
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: simple test request' for row in rows)
+        )
 
         # Текстовое поле по-прежнему приглашает добавить еще один элемент. Ввод "another test request"
-
+        self.fail('Закончить тест')
         # Страница обновляется и теперь показывает оба элемента списка
 
         # Проверяем сохранил ли сайт этот список. Видим, что сайт сгенерировал уникальный  url-адрес - об этом выводится
         # небольшой текст с объяснениями
 
         # Заходим на данный url и видим, что список по-прежнему там
+
 
 
 if __name__ =='__main__':
