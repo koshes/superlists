dd6f49d9e17039b5dd0e812f95910095e7f598e4
Yakov Volodin <ysvolodin@gmail.com>
2021-03-04 11:16:24 +0300

    Базовая вьюха теперь возвращает минимальный html

--- lists/tests.py
+++ lists/tests.py
@@ -1,9 +1,24 @@
+from django.urls import resolve
 from django.test import TestCase
+from django.http import HttpRequest
+
+from lists.views import home_page
+
+class HomePageTest(TestCase):
+    '''тест домашней страницы'''
+
+    def test_root_url_resolves_to_home_page_view(self):
+        '''тест: корневой url преобразуется в вьюху домашней страницы'''
+
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
 
 
-class SmokeTest(TestCase):
-    '''Тест на токсичность'''
-
-    def test_bad_maths(self):
-        '''тест: неправильные математические расчеты'''
-        self.assertEqual(1+1, 3)
+    def test_home_page_returns_correct_html(self):
+        '''тест: домашняя страница возвращает правильный html'''
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf-8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.endswith('</html>'))
