7ca5e9c67f81851baebd90a67fa75c3a8668547e
Yakov Volodin <ysvolodin@gmail.com>
2021-03-04 09:47:12 +0300

    Расширен первый ФТ в комментария, теперь используется unnittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,42 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    '''Тест нового пользователя'''
 
-assert 'Django' in browser.title
+    def setUp(self):
+        '''Установка'''
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        '''Демонтаж'''
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        '''тест: можно ли начать список и получить его позже'''
+
+        # Посещение домашней страницы приложения
+        self.browser.get('http://localhost:8000')
+
+        # Видим, что заголовок и шапка страницы говорят о to-do-list
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Закончить тест')
+
+        # Предложение ввести элемент списка
+
+        # Ввод в текстовом поле "simple test request"
+
+        # При подтверждении страница обновляется и теперь она содержит "simple test request" в качестве элемента списка
+
+        # Текстовое поле по-прежнему приглашает добавить еще один элемент. Ввод "another test request"
+
+        # Страница обновляется и теперь показывает оба элемента списка
+
+        # Проверяем сохранил ли сайт этот список. Видим, что сайт сгенерировал уникальный  url-адрес - об этом выводится
+        # небольшой текст с объяснениями
+
+        # Заходим на данный url и видим, что список по-прежнему там
+
+
+if __name__ =='__main__':
+    unittest.main(warnings='ignore')
